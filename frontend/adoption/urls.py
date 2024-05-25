089c64a59055eb84a8420cf29d1e6a0b21967826
alexa kyle <alexa.poblacion@yahoo.com>
2024-05-25 15:00:42 +0800

    project start

---
+++ frontend/adoption/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+
+    path('', views.home, name='home'),
+    path('shop', views.shop, name='shop'),
+    path('pet', views.pet, name='pet'),
+    path('book', views.book, name='book'),
+
+]
---
+++ frontend/adoption/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+
+import requests
+
+# Create your views here.
+
+def home(request):
+
+    return render(request, 'home/index.html')
+
+def shop(request):
+
+    return render(request, 'home/shop.html')
+
+def pet(request):
+
+    return render(request, 'home/pet.html')
+
+def book(request):
+
+    return render(request, 'home/book.html')
